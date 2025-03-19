4b9f9dca47209fa52cb24ab0188927c2fbbed19e
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-03-19 10:41:53 +0100

    solution

--- src/main/scala/Deque.scala
+++ src/main/scala/Deque.scala
@@ -1,5 +1,18 @@
-class Deque[T]:
-   def push(elem:T): Unit = ???
-   def pop: Option[T] = ???
-   def shift: Option[T] = ???
-   def unshift(elem:T): Unit = ???
+case class Deque[T]():
+   private var stack: List[T] = List.empty[T]
+
+   def push(elem: T): Unit =
+      stack = stack.prepended(elem)
+
+   def pop: Option[T] =
+      val ret = stack.headOption
+      stack = stack.tail
+      ret
+
+   def shift: Option[T] =
+      val ret = stack.lastOption
+      stack = stack.init
+      ret
+
+   def unshift(elem: T): Unit =
+      stack = stack.appended(elem)
